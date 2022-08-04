19d6aa86665d30691c7388d3f0785ae28fd9c357
Dominik Drexler <dominik.drexler@gmx.net>
2022-08-04 15:37:02 +0200

    added run script and scropion

---
+++ learning/d2l/experiments/run_scripts/sketch/visitall_1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH -J visitall_1_6_8
+#SBATCH -t 7-00:00:00
+#SBATCH -C fat --exclusive
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=dominik.drexler@liu.se
+
+DOMAIN=visitall
+EXPERIMENT=sketch
+WIDTH=1
+MAX_SKETCH_RULES=6
+COMPLEXITY=8
+
+EXPERIMENT_NAME=${DOMAIN}_${EXPERIMENT}_${WIDTH}_${MAX_SKETCH_RULES}_${COMPLEXITY}
+
+# Run a single task in the foreground.
+mkdir -p ${D2L_PATH}/workspace/${EXPERIMENT_NAME}
+./../../run.py ${DOMAIN}:${EXPERIMENT} -w ${WIDTH} -r ${MAX_SKETCH_RULES} -c ${COMPLEXITY} > "${D2L_PATH}/workspace/${EXPERIMENT_NAME}/run.log"
