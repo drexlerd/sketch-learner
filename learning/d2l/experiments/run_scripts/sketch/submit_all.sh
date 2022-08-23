f05309ecd1bdd42c9fc235f6c9fa6805dd92ac3e
Dominik Drexler <dominik.drexler@gmx.net>
2022-08-23 10:27:38 +0200

    changed project to have ressource on the cluster

--- learning/d2l/experiments/run_scripts/sketch/submit_all.sh
+++ learning/d2l/experiments/run_scripts/sketch/submit_all.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-sbatch -A snic2021-5-488 blocks_4_clear_0.sh
-sbatch -A snic2021-5-488 blocks_4_clear_1.sh
-sbatch -A snic2021-5-488 blocks_4_clear_2.sh
+sbatch -A snic2022-5-341 blocks_4_clear_0.sh
+sbatch -A snic2022-5-341 blocks_4_clear_1.sh
+sbatch -A snic2022-5-341 blocks_4_clear_2.sh
 
-sbatch -A snic2021-5-488 blocks_4_on_0.sh
-sbatch -A snic2021-5-488 blocks_4_on_1.sh
-sbatch -A snic2021-5-488 blocks_4_on_2.sh
+sbatch -A snic2022-5-341 blocks_4_on_0.sh
+sbatch -A snic2022-5-341 blocks_4_on_1.sh
+sbatch -A snic2022-5-341 blocks_4_on_2.sh
 
-sbatch -A snic2021-5-488 childsnack_0.sh
-sbatch -A snic2021-5-488 childsnack_1.sh
-sbatch -A snic2021-5-488 childsnack_2.sh
+sbatch -A snic2022-5-341 childsnack_0.sh
+sbatch -A snic2022-5-341 childsnack_1.sh
+sbatch -A snic2022-5-341 childsnack_2.sh
 
-sbatch -A snic2021-5-488 delivery_0.sh
-sbatch -A snic2021-5-488 delivery_1.sh
-sbatch -A snic2021-5-488 delivery_2.sh
+sbatch -A snic2022-5-341 delivery_0.sh
+sbatch -A snic2022-5-341 delivery_1.sh
+sbatch -A snic2022-5-341 delivery_2.sh
 
-sbatch -A snic2021-5-488 grid_0.sh
-sbatch -A snic2021-5-488 grid_1.sh
-sbatch -A snic2021-5-488 grid_2.sh
+sbatch -A snic2022-5-341 grid_0.sh
+sbatch -A snic2022-5-341 grid_1.sh
+sbatch -A snic2022-5-341 grid_2.sh
 
-sbatch -A snic2021-5-488 gripper_0.sh
-sbatch -A snic2021-5-488 gripper_1.sh
-sbatch -A snic2021-5-488 gripper_2.sh
+sbatch -A snic2022-5-341 gripper_0.sh
+sbatch -A snic2022-5-341 gripper_1.sh
+sbatch -A snic2022-5-341 gripper_2.sh
 
-sbatch -A snic2021-5-488 miconic_0.sh
-sbatch -A snic2021-5-488 miconic_1.sh
-sbatch -A snic2021-5-488 miconic_2.sh
+sbatch -A snic2022-5-341 miconic_0.sh
+sbatch -A snic2022-5-341 miconic_1.sh
+sbatch -A snic2022-5-341 miconic_2.sh
 
-sbatch -A snic2021-5-488 reward_0.sh
-sbatch -A snic2021-5-488 reward_1.sh
-sbatch -A snic2021-5-488 reward_2.sh
+sbatch -A snic2022-5-341 reward_0.sh
+sbatch -A snic2022-5-341 reward_1.sh
+sbatch -A snic2022-5-341 reward_2.sh
 
-sbatch -A snic2021-5-488 spanner_0.sh
-sbatch -A snic2021-5-488 spanner_1.sh
-sbatch -A snic2021-5-488 spanner_2.sh
+sbatch -A snic2022-5-341 spanner_0.sh
+sbatch -A snic2022-5-341 spanner_1.sh
+sbatch -A snic2022-5-341 spanner_2.sh
 
-sbatch -A snic2021-5-488 visitall_0.sh
-sbatch -A snic2021-5-488 visitall_1.sh
-sbatch -A snic2021-5-488 visitall_2.sh
+sbatch -A snic2022-5-341 visitall_0.sh
+sbatch -A snic2022-5-341 visitall_1.sh
+sbatch -A snic2022-5-341 visitall_2.sh
