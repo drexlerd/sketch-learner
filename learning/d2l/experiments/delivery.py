f3801f1ab9ee554204e445709609811486bfaa3c
Dominik Drexler <dominik.drexler@liu.se>
2022-10-09 12:32:45 +0200

    added to_dot prints for debugging

--- learning/d2l/experiments/delivery.py
+++ learning/d2l/experiments/delivery.py
@@ -64,8 +64,23 @@ def experiments():
         distance_numerical_complexity_limit=15,
         generate_concept_distance_numerical=True,
     )
+
+    exps["hierarchy_debug"] = update_dict(
+        strips_base,
+        pipeline="hierarchy_pipeline",
+        instances=training_instances(),
+        max_states_per_instance=5000,
+        distance_numerical_complexity_limit=15,
+        generate_concept_distance_numerical=True,
+        debug_features=["n_count(c_not(c_equal(r_primitive(at_g,0,1),r_primitive(at,0,1))))",  # 5
+                        "b_empty(c_primitive(empty,0))",  # 2
+                        "n_concept_distance(c_some(r_inverse(r_primitive(at,0,1)),c_primitive(truck,0)), r_primitive(adjacent,0,1), c_primitive(at_g,1))",  # 7
+                        "n_concept_distance(c_some(r_inverse(r_primitive(at,0,1)),c_primitive(truck,0)), r_primitive(adjacent,0,1), c_and(c_all(r_inverse(r_primitive(at_g,0,1)),c_bot),c_some(r_inverse(r_primitive(at,0,1)),c_primitive(package,0))))",  # 15
+        ],
+    )
     return exps
 
 
 def training_instances():
+    return [f"instance_3_2_0"]
     return [f"instance_{i}_{j}_{k}" for i in range(2,5) for j in range(1,3) for k in range(0,10) ]
