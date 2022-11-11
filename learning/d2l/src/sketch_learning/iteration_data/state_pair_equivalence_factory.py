19ba4b3dcf345bc6be8da4719fea3843f8637200
Dominik Drexler <dominik.drexler@liu.se>
2022-11-11 21:44:35 +0100

    tiny

--- learning/d2l/src/sketch_learning/iteration_data/state_pair_equivalence_factory.py
+++ learning/d2l/src/sketch_learning/iteration_data/state_pair_equivalence_factory.py
@@ -42,7 +42,7 @@ class StatePairEquivalenceFactory:
             r_idx_to_state_pairs = defaultdict(set)
             state_pair_to_r_idx = dict()
             for s_idx, tuple_graph in instance_data.tuple_graphs.items():
-                if instance_data.goal_distance_information.is_deadend(s_idx):
+                if not instance_data.goal_distance_information.is_alive(s_idx):
                     continue
                 # add conditions
                 conditions = self._make_conditions(policy_builder, policy_boolean_features, policy_numerical_features, instance_data.feature_valuations[s_idx])
