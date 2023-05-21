2a0a3719c49e0794e505fc53bbe9ed2ec6bcd207
Dominik Drexler <dominik.drexler@gmx.net>
2023-05-21 12:33:29 +0200

    changed timeout to 1 day

--- learning/experiments/scripts/blocks_3_1.sh
+++ learning/experiments/scripts/blocks_3_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH -J blocks_3_1
-#SBATCH -t 3-00:00:00
+#SBATCH -t 1-00:00:00
 #SBATCH -C thin --exclusive
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=dominik.drexler@liu.se
--- learning/experiments/scripts/blocks_4_clear_0.sh
+++ learning/experiments/scripts/blocks_4_clear_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH -J blocks_4_clear_0
-#SBATCH -t 3-00:00:00
+#SBATCH -t 1-00:00:00
 #SBATCH -C thin --exclusive
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=dominik.drexler@liu.se
